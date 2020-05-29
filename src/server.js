9f0e1d545ea9c212e9ae119db4fda6fa933b9e80
Alireza-HN <alireza.hn78@gmail.com>
2020-05-29 20:38:08 +0430

    exe2

--- src/server.js
+++ src/server.js
@@ -1,11 +1,21 @@
+//load requirements
 const http = require('http');
+const data = require('../data/json.json');
 
-http.createServer(function (req, res)
-{
+//config server
+http.createServer(function (req, res) {
     if (req.url === '/') {
         res.end("Hello World");
     }
+    //root-route configed
+
+    else if (req.url === '/api') {
+        res.end(JSON.stringify(data));
+    }
+    //api-route configed
+
     else {
         res.end("<h1>404</h1><h3>Not Found</h3>");
     }
-}).listen(3000);
+    //404-page configed
+}).listen(3000, () => console.log("Server is up on http://localhost:3000"));
